22b27aae32d69cd3a3b3f70a5eba79df13225c7f
thosoo <mail@thaison-nguyen.de>
2023-10-09 22:08:22 +0200

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -2,8 +2,12 @@ $directoryPath = (Get-Location).Path
 $name = Split-Path -Path $directoryPath -Leaf
 Get-ChildItem $directoryPath
 
+Write-Host "Starting Launcher Generator"
+$launcherGeneratorPath = "D:\a\$name\PortableApps.comLauncher\PortableApps.comLauncherGenerator.exe"
+Start-Process -FilePath $launcherGeneratorPath -ArgumentList "D:\a\$name\$name\$name" -NoNewWindow -Wait
+
 Write-Host "Starting Installer Generator"
-$installerGeneratorPath = "D:\a\$name\PortableApps.comInstaller\PortableApps.comInstaller.exe"
+$installerGeneratorPath = "D:\a\$name\$name\PortableApps.comInstaller\PortableApps.comInstaller.exe"
 Start-Process -FilePath $installerGeneratorPath -ArgumentList "D:\a\$name\$name" -NoNewWindow -Wait
 
 Get-ChildItem $directoryPath
